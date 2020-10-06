db5a6dc5ffff5dd80d647d95c44050282cbfb024
강우영 <@DESKTOP-92B6AE3>
2020-10-06 20:24:02 +0900

    good luck!!

--- src/kang/w/y/firstclass.java
+++ src/kang/w/y/firstclass.java
@@ -5,6 +5,7 @@ public class firstclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello wolrd");
+		System.out.println("good luck!!");
 	}
 
 }
